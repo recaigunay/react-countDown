91d5afbb94243025a86daaa4df927f225a39e1b8
recai <recai.gunay@gmail.com>
2018-10-05 23:36:25 +0300

    calculate remaining time

---
+++ src/clock.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+    }
+    ChangeDeadline(e) {
+        //debugger;
+        //e.preventDefault();
+        const input = document.getElementById("newdate");
+        const val = input.value;
+        input.value = "";
+        this.props.ChangeDeadline(val);
+    }
+    componentDidMount() {
+        setInterval(() => this.CalculateTime(), 1000);
+    }
+
+    AddZero(num) {
+        if (num < 10) return "0" + num;
+        else return num;
+    }
+    CalculateTime() {
+        //debugger;
+        const input = document.getElementById("newdate");
+        const dtNow = new Date();
+        let dtLast = new Date(this.props.deadline);
+        if (input.value !== null && input.value !== "") dtLast = new Date(input.value);
+        const diff = dtLast.getTime() - dtNow.getTime();
+        if (diff > 0) {
+            const seconds = this.AddZero(Math.floor((diff / 1000) % 60));
+            const minutes = this.AddZero(Math.floor((diff / 1000 / 60) % 60));
+            const hours = this.AddZero(Math.floor(diff / (1000 * 60 * 60) % 24));
+            const days = this.AddZero(Math.floor(diff / (1000 * 60 * 60 * 24)));
+
+            this.setState({
+                days: days,
+                minutes: minutes,
+                hours: hours,
+                seconds: seconds
+            })
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <div>CountDown to {this.props.deadline}</div>
+                <div className="Clock-Days">{this.state.days} Days</div>
+                <div className="Clock-Hours">{this.state.hours} Hours</div>
+                <div className="Clock-Minutes">{this.state.minutes} Minutes</div>
+                <div className="Clock-Seconds">{this.state.seconds} Seconds</div>
+                <div>
+                    <input id="newdate" placeholder='new date'></input>
+                    <button onClick={() => this.ChangeDeadline()}>Submit</button>
+                </div>
+
+            </div>
+        )
+    }
+
+}
+export default Clock;
